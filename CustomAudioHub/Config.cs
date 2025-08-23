a44ffcb6fd3898b44bf45c6d6ea602daca059ae4
SlejmUr <slejmur@protonmail.com>
2025-08-23 18:50:19 +0200

    Making this as a dependency instead of plugin

--- CustomAudioHub/Config.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CustomAudioHub;
-
-public sealed class Config
-{
-    public bool Debug { get; set; }
-    public string AudioFilePath { get; set; }
-}
---
+++ CustomAudioHub/Hub.cs
@@ -0,0 +1,15 @@
+﻿using LabApi.Loader.Features.Paths;
+
+namespace CustomAudioHub;
+
+public static class Hub
+{
+    public static readonly DirectoryInfo AudioHubDir = PathManager.Configs.CreateSubdirectory("global").CreateSubdirectory("AudioHub");
+    /// <summary>
+    /// Combines the file name with the file path, your file name doesn't need to contain .ogg, if it doesn't it'll be added for you.
+    /// </summary>
+    public static string MakeFilePath(string audioName = "ExampleAudio", string audioType = ".ogg")
+    {
+        return Path.Combine(AudioHubDir.FullName, audioName.Contains(audioType) ? audioName : $"{audioName}{audioType}");
+    }
+}
